d308b9171bced063bc6f51faadf2a96aea72a0fc
Ronen Reouveni <ronenreouveni@gmail.com>
2018-05-28 11:47:54 -0700

    2more

--- 01-data-structures/03-linked-lists/linked_list.rb
+++ 01-data-structures/03-linked-lists/linked_list.rb
@@ -4,27 +4,69 @@ class LinkedList
   attr_accessor :head
   attr_accessor :tail
 
-  # This method creates a new `Node` using `data`, and inserts it at the end of the list.
+  # This method inserts the node at the end of the list.
   def add_to_tail(node)
+    if self.head == nil
+      self.head = node
+    else
+      self.tail.next = node
+    end
+    self.tail = node
   end
 
   # This method removes the last node in the lists and must keep the rest of the list intact.
+  #question about this right here.
   def remove_tail
+    if self.head != nil && self.head.next != nil
+      x = self.head
+      while x.next != nil
+        y = x
+        x = x.next
+      end
+        y.next = nil
+      self.tail = y
+    else
+      self.tail = nil
+    end
   end
+
 
   # This method prints out a representation of the list.
   def print
+    node = @head
+    until node == nil do
+      puts node.data
+      node = node.next
+    end
   end
 
   # This method removes `node` from the list and must keep the rest of the list intact.
   def delete(node)
+    if @head == node
+      remove_front
+    elsif @tail == node
+      remove_tail
+    else
+      current_node = @head
+      until current_node.next == node
+        current_node = current_node.next
+      end
+      current_node.next = node.next
+    end
   end
 
   # This method adds `node` to the front of the list and must set the list's head to `node`.
   def add_to_front(node)
+    node.next = self.head
+    self.head = node
   end
 
   # This method removes and returns the first node in the Linked List and must set Linked List's head to the second node.
   def remove_front
+    x = self.head
+    self.head = x.next
+    x.next = nil
   end
+
+
 end
--- 01-data-structures/04-hashes-part-1/hashclass.rb
+++ 01-data-structures/04-hashes-part-1/hashclass.rb
@@ -1,27 +1,49 @@
+require_relative "hash_item"
+
 class HashClass
 
   def initialize(size)
     @items = Array.new(size)
+    @size = size
   end
 
   def []=(key, value)
+    item = HashItem.new(key, value)
+    i = index(key, @size)
+
+    while @items[i] != nil && value != @items[i].value
+      resize
+    end
+    @items[i] = item
   end
 
 
   def [](key)
+    item = @items[index(key, @size)]
+    item.nil? ? nil : item.value
   end
 
   def resize
+    @size *= 2
+    new_hash = Array.new(@size)
+    @items.each do |item|
+      if item != nil
+        new_hash[index(item.key, @size)] = item
+      end
+    end
+    @items = new_hash
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+    key.sum % size
   end
 
   # Simple method to return the number of items in the hash
   def size
+    @size
   end
 
 end
