82061e1b74879bc312cdf4416c63e2468f4c9a37
Ronen Reouveni <ronenreouveni@gmail.com>
2018-07-19 12:25:56 -0700

    complexity

---
+++ algorithms/complexity/text.rb
@@ -0,0 +1,12 @@
+1. 5g highspeed internet vs first generation
+2. first index
+3. half each time, log n
+4. n/2, so we would need the length of the array, but it would be inbetween the best and worst case.
+5. parabolic graph
+6. DNE
+7. squared function
+8. collection.length
+9. just a line
+10. log
+11. 1
+12. log 
